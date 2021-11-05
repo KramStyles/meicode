1cd88cf7146b489f08b7d816cf090dbe005641de
Michael Jamie <kramstyles@outlook.com>
2021-11-06 00:23:53 +0100

    finished challenge 1

--- app/src/main/java/com/example/meicode/MainActivity.java
+++ app/src/main/java/com/example/meicode/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.meicode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -21,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
         TextView txtHello = findViewById(R.id.txtMsg);
         EditText editText = findViewById(R.id.txtText);
         txtHello.setText(editText.getText().toString());
+    }
+
+    public void gotoChallange(View view){
+        Intent intent = new Intent(this, challange1.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/meicode/challange1.java
@@ -0,0 +1,31 @@
+package com.example.meicode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class challange1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challange1);
+    }
+    public void submitDetails(View view){
+        EditText txtFname = findViewById(R.id.txtFname);
+        EditText txtMname = findViewById(R.id.txtMname);
+        EditText txtSname = findViewById(R.id.txtSname);
+        TextView lblFname = findViewById(R.id.lblFname);
+        TextView lblMname = findViewById(R.id.lblMname);
+        TextView lblSname = findViewById(R.id.lblSname);
+
+        lblFname.setText(txtFname.getText().toString().toUpperCase());
+        lblMname.setText(txtMname.getText().toString().toUpperCase());
+        lblSname.setText(txtSname.getText().toString().toUpperCase());
+    }
+}
