72e9bf21df4c992cd5b95c2f6607f23240414fbb
Michael Jamie <kramstyles@outlook.com>
2021-11-21 21:57:47 +0100

    adding button 2

--- app/src/main/java/com/example/meicode/third_ui.java
+++ app/src/main/java/com/example/meicode/third_ui.java
@@ -3,6 +3,8 @@ package com.example.meicode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class third_ui extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class third_ui extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_ui);
+    }
+
+    public void thirdSayHello(View view) {
+        TextView txtWelcome = findViewById(R.id.thirdTxtWelcome);
+        txtWelcome.setText("Hello and Welcome");
     }
 }
