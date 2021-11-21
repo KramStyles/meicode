bf52cb1bd941066cb7870a7d1cf5707c4bdf61c3
Michael Jamie <kramstyles@outlook.com>
2021-11-21 22:21:09 +0100

    using switch case for click listener

--- app/src/main/java/com/example/meicode/third_ui.java
+++ app/src/main/java/com/example/meicode/third_ui.java
@@ -4,18 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class third_ui extends AppCompatActivity {
+public class third_ui extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_ui);
+        Button btnHi = findViewById(R.id.thirdBtnHi);
+        TextView txtWelcome = findViewById(R.id.thirdTxtWelcome);
+
+//        Calling click method using the switch case
+        Button btnDiffMthd = findViewById(R.id.thirdBtnDifferentClickMethod);
+        btnDiffMthd.setOnClickListener(this);
+
+
+        btnHi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Say hello in console");
+            }
+        });
     }
 
     public void thirdSayHello(View view) {
         TextView txtWelcome = findViewById(R.id.thirdTxtWelcome);
         txtWelcome.setText("Hello and Welcome");
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView txtWelcome = findViewById(R.id.thirdTxtWelcome);
+        switch (v.getId()){
+            case R.id.btnHello:
+                System.out.println("Hello clicked");
+                break;
+            case R.id.thirdBtnDifferentClickMethod:
+                txtWelcome.setText("Method from Another method Called!");
+            default:
+                System.out.println("I don't know");
+        }
     }
 }
