09056b064cbb55524be9cfde21c3731b89fe44b9
Michael Jamie <kramstyles@outlook.com>
2021-11-22 09:41:18 +0100

    added ui 2 page

--- app/src/main/java/com/example/meicode/UiBasics.java
+++ app/src/main/java/com/example/meicode/UiBasics.java
@@ -2,6 +2,7 @@ package com.example.meicode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -20,9 +21,11 @@ public class UiBasics extends AppCompatActivity implements View.OnClickListener 
 
         txtToast = findViewById(R.id.uiTxtToast);
         Button btnToast = findViewById(R.id.uiBtnToast);
+        Button btnBasic = findViewById(R.id.uiBtnGotoBasic2);
 
 
         btnToast.setOnClickListener(this);
+        btnBasic.setOnClickListener(this);
         btnToast.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -37,6 +40,11 @@ public class UiBasics extends AppCompatActivity implements View.OnClickListener 
         switch (v.getId()){
             case R.id.uiBtnToast:
                 Toast.makeText(this, "Music is playing in your head ".concat(txtToast.getText().toString().toUpperCase()), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.uiBtnGotoBasic2:
+                Intent intent = new Intent(this, UiBasics2.class);
+                startActivity(intent);
+                System.out.println("This should work");
                 break;
             default:
                 System.out.println("I don't understand ooo");
