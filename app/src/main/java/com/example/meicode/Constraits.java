8aa0387227d39c03bb473b5276e77dc71cb0d4e6
Michael Jamie <kramstyles@outlook.com>
2021-11-24 15:51:50 +0100

    Constraits layout

---
+++ app/src/main/java/com/example/meicode/Constraits.java
@@ -0,0 +1,14 @@
+package com.example.meicode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Constraits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraits);
+    }
+}
--- app/src/main/java/com/example/meicode/Layouts.java
+++ app/src/main/java/com/example/meicode/Layouts.java
@@ -2,13 +2,30 @@ package com.example.meicode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Layouts extends AppCompatActivity {
+public class Layouts extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layouts);
+
+        btnHello = findViewById(R.id.layBtnHello);
+        btnHello.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.layBtnHello:
+                Intent intent = new Intent(this, Constraits.class);
+                startActivity(intent);
+        }
     }
 }
