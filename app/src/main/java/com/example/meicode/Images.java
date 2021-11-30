4b55232789af356721f949e6bcbc7b55367087b6
Michael Jamie <kramstyles@outlook.com>
2021-11-30 22:18:21 +0100

    started Spinner and List view

--- app/src/main/java/com/example/meicode/Images.java
+++ app/src/main/java/com/example/meicode/Images.java
@@ -2,13 +2,29 @@ package com.example.meicode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Images extends AppCompatActivity {
+public class Images extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images);
+
+        btnSpinner = findViewById(R.id.btnSpinner);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSpinner:
+                Intent intent = new Intent(this, SpinnerList.class);
+                startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/meicode/SpinnerList.java
@@ -0,0 +1,14 @@
+package com.example.meicode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpinnerList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner_list);
+    }
+}
